d838acb32720c93bafbcca093f3b4165a4c3840b
Owen Quinlan <hello@owenscode.org>
2019-04-28 22:43:21 +1000

    added console app V1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string AppVersion = "v1.0.0";
+            Console.WriteLine(AppVersion);
+            string test = Console.ReadLine();
+
+        }
+    }
+}
